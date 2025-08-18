ace928e164108332d4a2ba2aaf747c7db5444bbd
yjiang@stackline.com <yjiang@stackline.com>
2025-08-17 20:24:27 -0700

    add notebooks

---
+++ notebook/test.py
@@ -0,0 +1,71 @@
+"""run.py:"""
+#!/usr/bin/env python
+import os
+import sys
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+"""Non-blocking point-to-point communication."""
+
+""" All-Reduce example."""
+def run(rank, size):
+    """ Simple collective communication. """
+    group = dist.new_group([0, 1])
+    tensor = torch.ones(1)
+    dist.all_reduce(tensor, op=dist.ReduceOp.SUM, group=group)
+    print('Rank ', rank, ' has data ', tensor[0])
+
+
+def init_process(rank, size, fn, backend='gloo'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+
+""" Implementation of a ring-reduce with addition. """
+def allreduce(send, recv):
+   rank = dist.get_rank()
+   size = dist.get_world_size()
+   send_buff = send.clone()
+   recv_buff = send.clone()
+   accum = send.clone()
+
+   left = ((rank - 1) + size) % size
+   right = (rank + 1) % size
+
+   for i in range(size - 1):
+       if i % 2 == 0:
+           # Send send_buff
+           send_req = dist.isend(send_buff, right)
+           dist.recv(recv_buff, left)
+           accum[:] += recv_buff[:]
+       else:
+           # Send recv_buff
+           send_req = dist.isend(recv_buff, right)
+           dist.recv(send_buff, left)
+           accum[:] += send_buff[:]
+       send_req.wait()
+   recv[:] = accum[:]
+
+   
+
+if __name__ == "__main__":
+    world_size = 2
+    processes = []
+    if "google.colab" in sys.modules:
+        print("Running in Google Colab")
+        mp.get_context("spawn")
+    else:
+        mp.set_start_method("spawn")
+    for rank in range(world_size):
+        p = mp.Process(target=init_process, args=(rank, world_size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
